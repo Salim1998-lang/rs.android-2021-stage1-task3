666db93abbc3d9ad6f0695acfcad4e5304ce9e63
Salim <vsalim1998@gmail.com>
2021-07-08 12:31:14 +0300

    Solve

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,23 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        TODO()
+        val a = array[0].toLong()
+        val b = array[1].toLong()
+        for (i in 1..b) {
+            if (a == (factorial(b)/(factorial(i)*factorial(b-i)))) {
+                return i.toInt()
+            }
+        }
+        return null
     }
+
+    private fun factorial(int: Long): Long {
+        var f = 1L
+        for (i in 1..int) {
+            f *= i
+        }
+        return f
+    }
+
+
 }
