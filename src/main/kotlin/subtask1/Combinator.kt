38cd81d408b763291ed9045a9f229fbc558a2f8a
Salim <vsalim1998@gmail.com>
2021-07-11 16:45:24 +0300

    Solve

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -21,6 +21,4 @@ class Combinator {
         }
         return f
     }
-
-
 }
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,6 +4,28 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val square = number.toLong() * number.toLong()
+        val list = decomposer(number.toLong(), square)
+
+        return list?.toTypedArray()
+    }
+
+    private fun decomposer(number: Long, squareNumber: Long): ArrayList<Int>? {
+        if (squareNumber == 0L) return arrayListOf()
+        var list: ArrayList<Int>? = null
+
+        for (i in number - 1 downTo 1) {
+            val num = squareNumber - i * i
+            if (num >= 0) {
+                list = decomposer(i, num)
+                list?.add(i.toInt())
+            }
+
+            if (list != null) {
+                return list
+            }
+        }
+
+        return list
     }
 }
